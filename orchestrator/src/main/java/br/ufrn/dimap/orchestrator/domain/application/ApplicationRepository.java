42857131f0310c461dcc956b3179c3388e91e2e9
Daniel Smith <danielsmithmxp@gmail.com>
2018-12-01 17:36:39 -0200

    Google cloud basic integration test

--- orchestrator/src/main/java/br/ufrn/dimap/orchestrator/domain/application/ApplicationRepository.java
+++ orchestrator/src/main/java/br/ufrn/dimap/orchestrator/domain/application/ApplicationRepository.java
@@ -17,60 +17,61 @@ import java.util.List;
 import java.util.Objects;
 
 @Repository
-public class ApplicationRepository{
+public class ApplicationRepository {
 
-	private final Datastore datastore;
-	private final KeyFactory keyFactory;
+    private final Datastore datastore;
+    private final KeyFactory keyFactory;
 
-	@Autowired
-	public ApplicationRepository(Datastore datastore) {
-		this.datastore = Objects.requireNonNull(datastore,"The datastore object could not be null.");
-		this.keyFactory = datastore.newKeyFactory().setKind("App");
-	}
-	
-	public Application findByAppspot(Appspot serverAppspot) throws ApplicationNotFoundException {
-		
-		Query<Entity> query = Query.newEntityQueryBuilder()
-				.setKind("App")
-				.setFilter(PropertyFilter.eq("__key__", 
-						keyFactory.newKey(serverAppspot.getAppspotName())))
-				.build();
-		QueryResults<Entity> apps = datastore.run(query);
-		Application appReturn = null;
-		while (apps.hasNext()) {
-			Entity app = apps.next();
-			appReturn = new Application();
-			//TODO populate app
-		}
-		return appReturn;
-	}
+    @Autowired
+    public ApplicationRepository(Datastore datastore) {
+        this.datastore = Objects.requireNonNull(datastore, "The datastore object could not be null.");
+        this.keyFactory = datastore.newKeyFactory().setKind("Application");
+    }
 
-	public List<Application> findAllApplications() {
-		return null;
-	}
+    public Application findByAppspot(Appspot serverAppspot) throws ApplicationNotFoundException {
 
-	public Application save(Application application) {	
-		Key key = keyFactory.newKey(application.getAppspot().getAppspotName());
-		Entity app = Entity.newBuilder(key)
-		.set("appspot", application.getAppspot().getAppspotName())
-		.set("owner", application.getOwnerName())
-		.build();
-		// TODO add services etc
-		//datastore.put(app);
-		// TODO check how to retrieve app id
-		return application;
-	}
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind("App")
+                .setFilter(PropertyFilter.eq("__key__",
+                        keyFactory.newKey(serverAppspot.getAppspotName())))
+                .build();
 
-	public void remove(Appspot appspot) {
-		Key key = keyFactory.newKey(appspot.getAppspotName());
-		datastore.delete(key);
-	}
+        QueryResults<Entity> apps = datastore.run(query);
+        Application appReturn = null;
+        while (apps.hasNext()) {
+            Entity app = apps.next();
+            appReturn = new Application();
+            //TODO populate app
+        }
+        return appReturn;
+    }
 
-	public Application removeProvidedService(int providedServiceId) {
-		return null;
-	}
+    public List<Application> findAllApplications() {
+        return null;
+    }
 
-	public boolean existsApplicationWithAppspot(Appspot appspot) {
-		return false;
-	}
+    public Application save(Application application) {
+        Key key = keyFactory.newKey(application.getAppspot().getAppspotName());
+        Entity app = Entity.newBuilder(key)
+                .set("appspot", application.getAppspot().getAppspotName())
+                .set("owner", application.getOwnerName())
+                .build();
+        // TODO add services etc
+        datastore.put(app);
+        // TODO check how to retrieve app id
+        return application;
+    }
+
+    public void remove(Appspot appspot) {
+        Key key = keyFactory.newKey(appspot.getAppspotName());
+        datastore.delete(key);
+    }
+
+    public Application removeProvidedService(int providedServiceId) {
+        return null;
+    }
+
+    public boolean existsApplicationWithAppspot(Appspot appspot) {
+        return false;
+    }
 }
---
+++ orchestrator/src/test/java/br/ufrn/dimap/orchestrator/token/ApplicationIntegrationTests.java
@@ -0,0 +1,53 @@
+package br.ufrn.dimap.orchestrator.token;
+
+
+import br.ufrn.dimap.orchestrator.OrchestratorApplication;
+import br.ufrn.dimap.orchestrator.configuration.GoogleCloudConfiguration;
+import br.ufrn.dimap.orchestrator.domain.application.Application;
+import br.ufrn.dimap.orchestrator.domain.application.ApplicationRepository;
+import br.ufrn.dimap.orchestrator.domain.application.Appspot;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations="classpath:application.properties")
+@ContextConfiguration(classes = {OrchestratorApplication.class})
+public class ApplicationIntegrationTests {
+
+    @Autowired
+    private ApplicationRepository repository;
+
+    @Test
+    public void testFindAllApplications(){
+        List<Application> allApplications = repository.findAllApplications();
+
+        assertNotNull(allApplications);
+    }
+
+    @Test
+    public void testSaveApplication(){
+        Application application = new Application();
+
+        application.setAppspot(Appspot.from("Appspot test"));
+        application.setOwnerName("Ownername test");
+
+        Application applicationAfterSave = repository.save(application);
+
+        assertNotNull(applicationAfterSave);
+        assertNotNull(application.getId());
+        assertNotNull(application.getAppspot());
+        assertNotNull(application.getOwnerName());
+    }
+
+}
