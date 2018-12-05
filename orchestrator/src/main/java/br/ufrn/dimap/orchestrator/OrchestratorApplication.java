10680672afd3627b241cf4c25f58f7f9c5f99589
Daniel Costa Smith de Medeiros <danielsmith@info.ufrn.br>
2018-12-05 14:33:00 -0300

    Deploying to the cloud

--- orchestrator/src/main/java/br/ufrn/dimap/orchestrator/OrchestratorApplication.java
+++ orchestrator/src/main/java/br/ufrn/dimap/orchestrator/OrchestratorApplication.java
@@ -2,9 +2,10 @@ package br.ufrn.dimap.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrchestratorApplication {
+public class OrchestratorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestratorApplication.class, args);
