1568efb76c55a05f72ea4b58502646282cab9923
Daniel Smith <danielsmithmxp@gmail.com>
2018-12-09 21:01:35 -0200

    Adding realtime ranking

---
+++ orchestrator/src/main/java/br/ufrn/dimap/orchestrator/domain/ranking/Ranking.java
@@ -0,0 +1,15 @@
+package br.ufrn.dimap.orchestrator.domain.ranking;
+
+public class Ranking {
+
+    private String data;
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ orchestrator/src/main/java/br/ufrn/dimap/orchestrator/web/controller/ranking/RankingController.java
@@ -0,0 +1,34 @@
+package br.ufrn.dimap.orchestrator.web.controller.ranking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/ranking")
+public class RankingController {
+
+    private final RankingSubscriberManager rankingSubscriberManager;
+
+    @Autowired
+    public RankingController(RankingSubscriberManager rankingSubscriberManager) {
+        this.rankingSubscriberManager = rankingSubscriberManager;
+    }
+
+
+    @RequestMapping("/index")
+    public String index(){
+        return "ranking/index";
+    }
+
+    @RequestMapping("/realTime")
+    public SseEmitter registerRealtimeUpdate(HttpSession httpSession){
+        String sessionId = httpSession.getId();
+
+        return rankingSubscriberManager.registerSubscriber(sessionId);
+    }
+
+}
