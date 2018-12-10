6382b27b3518070f7629d55cde1d0ba8b8c78e46
Vitor Greati <greati@ufrn.edu.br>
2018-12-10 13:54:37 -0300

    Capture date

--- orchestrator/src/main/java/br/ufrn/dimap/orchestrator/web/controller/ranking/RankingController.java
+++ orchestrator/src/main/java/br/ufrn/dimap/orchestrator/web/controller/ranking/RankingController.java
@@ -1,9 +1,18 @@
 package br.ufrn.dimap.orchestrator.web.controller.ranking;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.google.type.Date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.http.HttpSession;
 
@@ -25,9 +34,16 @@ public class RankingController {
     }
 
     @RequestMapping("/realTime")
-    public SseEmitter registerRealtimeUpdate(HttpSession httpSession){
-        String sessionId = httpSession.getId();
+    public SseEmitter registerRealtimeUpdate(
+    		HttpSession httpSession,
+    		@RequestParam(value="since", required=false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME) LocalDateTime since){
         
+    	String sessionId = httpSession.getId();
+        
+        if (since != null) {
+        	// use this date for report gen
+        }
+
         return rankingSubscriberManager.registerSubscriber(sessionId);
     }
 
